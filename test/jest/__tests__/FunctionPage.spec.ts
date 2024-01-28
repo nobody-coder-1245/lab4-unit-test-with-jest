360be725f96e8db584ee97cb764a09ebe9278343
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-01-28 23:55:04 +0700

    add: add unit test to function-page & write test logic for checking name

---
+++ test/jest/__tests__/FunctionPage.spec.ts
@@ -0,0 +1,54 @@
+import { describe, expect, it } from '@jest/globals';
+import { installQuasarPlugin } from '@quasar/quasar-app-extension-testing-unit-jest';
+import { shallowMount } from '@vue/test-utils';
+import FunctionPage from 'src/pages/FunctionPage.vue';
+import { shallowMount } from '@vue/test-utils';
+
+// Specify here Quasar config you'll need to test your component
+installQuasarPlugin();
+
+describe('IndexPage', () => {
+  it('should render correct contents', () => {
+    const wrapper = shallowMount(FunctionPage);
+    const header = wrapper.find('.htmlClass h1');
+    expect(header.exists()).toBe(true);
+    expect(header.text()).toBe('Vue is awesome.');
+  });
+});
+
+it('check text content to be as defined in variable', () => {
+  const wrapper = shallowMount(FunctionPage, {
+    data() {
+      return {
+        title: 'I love Vue.',
+      };
+    },
+  });
+  const header = wrapper.find('.htmlClass h1');
+  expect(header.text()).toBe('I love Vue.');
+});
+
+test('should show the form element on the user output', () => {
+  const wrapper = shallowMount(FunctionPage);
+  expect(wrapper.find('form').exists()).toBe(true);
+});
+
+test('should contain input fields in template', () => {
+  const wrapper = shallowMount(FunctionPage);
+  expect(wrapper.find('form > input').exists()).toBe(true);
+});
+
+test('should have button', () => {
+  const wrapper = shallowMount(FunctionPage);
+  expect(wrapper.find('button').exists()).toBe(true);
+});
+
+// create test for the function addname
+test('trigger click event on button name and code', async () => {
+  const wrapper = shallowMount(FunctionPage);
+  const button = wrapper.find('button');
+  await button.trigger('click');
+  expect(wrapper.vm.addname('Traiphakh', 'Sittikaew', '6404101318')).toBe(
+    'Traiphakh Sittikaew 6404101318'
+  );
+});
